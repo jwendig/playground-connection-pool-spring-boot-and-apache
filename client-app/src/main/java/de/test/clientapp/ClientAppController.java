44277b43abbca7096c2613897401c0e719a6f355
Johannes Wendig <johannes.wendig@outletcity.com>
2022-01-21 15:26:50 +0100

    feat: add a post endpoint to server-app and a endpoint to the client-app which calls it to have a endpoint which will not be retried by the apache connection-pool

--- client-app/src/main/java/de/test/clientapp/ClientAppController.java
+++ client-app/src/main/java/de/test/clientapp/ClientAppController.java
@@ -3,6 +3,7 @@ package de.test.clientapp;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,10 +19,16 @@ public class ClientAppController {
         return "hey i am the client-app";
     }
 
-    @GetMapping("/request-to-server-app")
-    public String requestToServerApp(){
+    @GetMapping("/request-to-server-app-via-get")
+    public String requestToServerAppViaGet(){
             String responseFromServerApp = restTemplate.getForObject("http://server-apache/hello", String.class);
             return "answer from server-app: " + responseFromServerApp;
+    }
+
+    @GetMapping("/request-to-server-app-via-post")
+    public String requestToServerAppViaPost(){
+        String responseFromServerApp = restTemplate.postForObject("http://server-apache/hello", ":P", String.class);
+        return "answer from server-app: " + responseFromServerApp;
     }
 
 }
--- server-app/src/main/java/de/test/serverapp/ServerAppController.java
+++ server-app/src/main/java/de/test/serverapp/ServerAppController.java
@@ -1,14 +1,21 @@
 package de.test.serverapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServerAppController {
 
     @GetMapping("/hello")
-    public String simple(){
+    public String simpleGet(){
         return "hey i am the server-app";
+    }
+
+    @PostMapping("/hello")
+    public String simplePost(@RequestBody String body){
+        return "hey i am the server-app - you sent: " + body;
     }
 
 }
