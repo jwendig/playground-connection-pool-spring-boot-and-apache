df465d7da20d2c9656525d6ee6d9761373e513f7
Johannes Wendig <johannes.wendig@outletcity.com>
2022-01-21 13:44:17 +0100

    chore: configure client-app to make request to server-app through apache

---
+++ client-app/src/main/java/de/test/clientapp/ClientAppController.java
@@ -0,0 +1,35 @@
+package de.test.clientapp;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ClientAppController {
+
+    private final RestTemplate restTemplate;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hey i am the client-app";
+    }
+
+    @GetMapping("/request-to-server-app")
+    public String requestToServerApp(){
+        try {
+            String responseFromServerApp = restTemplate.getForObject("http://server-apache/test", String.class);
+            return "answer from server-app: " + responseFromServerApp;
+        }catch (Exception e){
+            log.error("fuck", e);
+
+            return "error from server-app: " + e.getMessage();
+        }
+
+
+    }
+
+}
